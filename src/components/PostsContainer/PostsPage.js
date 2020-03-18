2ff9e9d8ea1cce532fefc79d6da6315523a99a86
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-18 14:34:20 -0500

    I am working through setting it all up

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,13 +3,19 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
+import { useState } from "react";
 // import data 
+import dummyData from "../../dummy-data"
 
 const PostsPage = () => {
   // set up state for your data
+  const [dummyData] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      /* map through data here to return a Post and pass data as props to Post */
+      {data.map(p => (
+        <ProfileCard propsPassedFromParent={p} />
+      ))}
     </div>
   );
 };
